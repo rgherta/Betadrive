1febfcd31754bbedb6f200873a108ac3bc89e8cd
Roman Gherta <roman.gherta@gmail.com>
2019-04-14 01:01:17 +0200

    Backend integration

--- app/src/main/java/com/example/betadrive/ResponseActivity.java
+++ app/src/main/java/com/example/betadrive/ResponseActivity.java
@@ -9,16 +9,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.betadrive.Utils.NetworkUtils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.google.maps.android.PolyUtil;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.List;
 
 
 public class ResponseActivity extends AppCompatActivity {
@@ -34,6 +48,7 @@ public class ResponseActivity extends AppCompatActivity {
     Address destAddress;
     String payment;
     JSONObject mRequest;
+    TextView mResponse;
 
 
     @Override
@@ -50,25 +65,24 @@ public class ResponseActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
 
 
-        TextView response = findViewById(R.id.response);
+        mResponse = findViewById(R.id.response);
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        currentUser.getIdToken(false).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
-            @Override
-            public void onComplete(@NonNull Task<GetTokenResult> task) {
+        currentUser.getIdToken(false).addOnCompleteListener(task -> {
 
-                if (task.isSuccessful()) {
+            if (task.isSuccessful()) {
 
-                    Log.w(TAG,"Token found single thread after force refresh "+task.getResult().getToken());
-                    //response.setText(task.getResult().getToken());
-                    String token = task.getResult().getToken();
-                    mRequest = createRequest(pickupAddress, destAddress, payment, token );
+                Log.w(TAG,"Token found single thread after force refresh "+task.getResult().getToken());
+                //response.setText(task.getResult().getToken());
+                String token = task.getResult().getToken();
+                mRequest = createRequest(pickupAddress, destAddress, payment, token );
+                URL hailUrl = NetworkUtils.buildHailUrl("api", "hail");
+                queue.add( makeJsonRequest(Request.Method.POST, hailUrl, mRequest) );
 
 
 
-                }
             }
         });
 
@@ -103,6 +117,23 @@ public class ResponseActivity extends AppCompatActivity {
         return response;
 
     }
+
+
+
+    private JsonObjectRequest makeJsonRequest(int method, URL url, JSONObject myRequest){
+
+       return new JsonObjectRequest(method, url.toString()
+                                    , myRequest
+                                    , response -> mResponse.setText("Response: " + response.toString())
+               , error -> {
+                   // TODO: Handle error
+                   Log.w(TAG, "Json request error");
+
+               });
+
+
+    }
+
 
 
     @Override
--- app/src/main/java/com/example/betadrive/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/betadrive/Utils/NetworkUtils.java
@@ -15,6 +15,7 @@ import java.net.URL;
 public class NetworkUtils {
 
     private static final String DIRECTIONS_BASE = "https://maps.googleapis.com/maps/api/directions";
+    private static final String FUNCTIONS_BASE = "https://us-central1-beta-94f76.cloudfunctions.net";
 
     public static URL buildDirectionsUrl(String apiType, String origin, String destination, String API_KEY){
     Uri builtUri = Uri.parse(DIRECTIONS_BASE).buildUpon()
@@ -34,5 +35,25 @@ public class NetworkUtils {
         return url;
 
     }
+
+
+
+    public static URL buildHailUrl(String apiBase, String hailEndpoint){
+        Uri builtUri = Uri.parse(FUNCTIONS_BASE).buildUpon()
+                .appendPath(apiBase)
+                .appendPath(hailEndpoint)
+                .build();
+        URL url = null;
+
+        try{
+            url = new URL(builtUri.toString());
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return url;
+
+    }
+
 
 }
